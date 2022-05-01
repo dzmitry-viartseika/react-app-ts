fafb25c8ada683279b2e8ac46bdb29ebbd92f66c
Dmitry Verteyko <verteyko1990@gmail.com>
2022-05-01 13:10:29 +0300

    added useEffects

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TheButton, Ptag } from "./components/";
 
 function App(): JSX.Element {
+    const [counter, setCounter] = useState<number>(0);
+
+    const handleClick = () => {
+        setCounter(counter + 1);
+    };
+
+    useEffect(() => {
+    }, [counter]);
   return (
     <div className={'App'}>
+        counter={ counter }
         <TheButton
             children={`primary`}
             appearance={`primary`}
+            onClick={handleClick}
         />
 
         <TheButton
